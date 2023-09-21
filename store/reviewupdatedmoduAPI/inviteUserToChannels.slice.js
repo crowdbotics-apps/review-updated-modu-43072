6e0950f92958386c3097e991523682f22d4fc77d
Crowdbotics <engineering@crowdbotics.com>
2023-09-21 13:28:04 +0000

    Update store.

--- store/reviewupdatedmoduAPI/inviteUserToChannels.slice.js
+++ store/reviewupdatedmoduAPI/inviteUserToChannels.slice.js
@@ -1,41 +1,49 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { apiService } from "./api";
-export const modules_slack_service_invite_user_to_channel_create = createAsyncThunk("inviteUserToChannels/modules_slack_service_invite_user_to_channel_create", async payload => {
-  const response = await apiService.modules_slack_service_invite_user_to_channel_create(payload);
-  return response.data;
-});
-const initialState = {
-  entities: [],
-  api: {
-    loading: "idle",
-    error: null
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const modules_slack_service_invite_user_to_channel_create = createAsyncThunk(
+  "inviteUserToChannels/modules_slack_service_invite_user_to_channel_create",
+  async payload => {
+    const response = await apiService.modules_slack_service_invite_user_to_channel_create(
+      payload
+    )
+    return response.data
   }
-};
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
 const inviteUserToChannelsSlice = createSlice({
   name: "inviteUserToChannels",
   initialState,
   reducers: {},
   extraReducers: {
-    [modules_slack_service_invite_user_to_channel_create.pending]: (state, action) => {
+    [modules_slack_service_invite_user_to_channel_create.pending]: (
+      state,
+      action
+    ) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending";
+        state.api.loading = "pending"
       }
     },
-    [modules_slack_service_invite_user_to_channel_create.fulfilled]: (state, action) => {
+    [modules_slack_service_invite_user_to_channel_create.fulfilled]: (
+      state,
+      action
+    ) => {
       if (state.api.loading === "pending") {
-        state.entities.push(action.payload);
-        state.api.loading = "idle";
+        state.entities.push(action.payload)
+        state.api.loading = "idle"
       }
     },
-    [modules_slack_service_invite_user_to_channel_create.rejected]: (state, action) => {
+    [modules_slack_service_invite_user_to_channel_create.rejected]: (
+      state,
+      action
+    ) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
+        state.api.error = action.error
+        state.api.loading = "idle"
       }
     }
   }
-});
+})
 export default {
   modules_slack_service_invite_user_to_channel_create,
   slice: inviteUserToChannelsSlice
-};
+}
--- store/reviewupdatedmoduAPI/oktaUserLogins.slice.js
+++ store/reviewupdatedmoduAPI/oktaUserLogins.slice.js
@@ -1,32 +1,30 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import { apiService } from "./api"
-export const rest_auth_password_reset_confirm_create = createAsyncThunk(
-  "passwordResetConfirms/rest_auth_password_reset_confirm_create",
+export const modules_okta_login_create = createAsyncThunk(
+  "oktaUserLogins/modules_okta_login_create",
   async payload => {
-    const response = await apiService.rest_auth_password_reset_confirm_create(
-      payload
-    )
+    const response = await apiService.modules_okta_login_create(payload)
     return response.data
   }
 )
 const initialState = { entities: [], api: { loading: "idle", error: null } }
-const passwordResetConfirmsSlice = createSlice({
-  name: "passwordResetConfirms",
+const oktaUserLoginsSlice = createSlice({
+  name: "oktaUserLogins",
   initialState,
   reducers: {},
   extraReducers: {
-    [rest_auth_password_reset_confirm_create.pending]: (state, action) => {
+    [modules_okta_login_create.pending]: (state, action) => {
       if (state.api.loading === "idle") {
         state.api.loading = "pending"
       }
     },
-    [rest_auth_password_reset_confirm_create.fulfilled]: (state, action) => {
+    [modules_okta_login_create.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
         state.entities.push(action.payload)
         state.api.loading = "idle"
       }
     },
-    [rest_auth_password_reset_confirm_create.rejected]: (state, action) => {
+    [modules_okta_login_create.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
         state.api.error = action.error
         state.api.loading = "idle"
@@ -34,7 +32,4 @@ const passwordResetConfirmsSlice = createSlice({
     }
   }
 })
-export default {
-  rest_auth_password_reset_confirm_create,
-  slice: passwordResetConfirmsSlice
-}
+export default { modules_okta_login_create, slice: oktaUserLoginsSlice }
