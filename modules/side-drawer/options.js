c0233079fb25cfa4c5b33e106984952fc8ce36b4
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-09-21 23:22:58 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ modules/side-drawer/options.js
@@ -0,0 +1,17 @@
+const screenSet = [];
+const drawerOptions = {
+  initialRouteName: "",
+  drawerType: "",
+  screenOptions: {},
+  drawerPosition: "",
+  drawerStyle: {},
+  overlayColor: "",
+  hideStatusBar: false,
+  edgeWidth: 0,
+  keyboardDismissMode: "",
+  minSwipeDistance: 0
+};
+export default {
+  screenSet: screenSet,
+  drawerOptions: drawerOptions
+};
