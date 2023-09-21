d35258373c206d9b1af7fe9b4ff3e5897dd00887
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-09-21 17:18:50 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/reviewupdatedmoduAPI/updateCampaignSettings.slice.js
+++ store/reviewupdatedmoduAPI/updateCampaignSettings.slice.js
@@ -1,49 +1,41 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { apiService } from "./api"
-export const modules_mailchimp_campaigns_update_campaign_settings_create = createAsyncThunk(
-  "updateCampaignSettings/modules_mailchimp_campaigns_update_campaign_settings_create",
-  async payload => {
-    const response = await apiService.modules_mailchimp_campaigns_update_campaign_settings_create(
-      payload
-    )
-    return response.data
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiService } from "./api";
+export const modules_mailchimp_campaigns_update_campaign_settings_create = createAsyncThunk("updateCampaignSettings/modules_mailchimp_campaigns_update_campaign_settings_create", async payload => {
+  const response = await apiService.modules_mailchimp_campaigns_update_campaign_settings_create(payload);
+  return response.data;
+});
+const initialState = {
+  entities: [],
+  api: {
+    loading: "idle",
+    error: null
   }
-)
-const initialState = { entities: [], api: { loading: "idle", error: null } }
+};
 const updateCampaignSettingsSlice = createSlice({
   name: "updateCampaignSettings",
   initialState,
   reducers: {},
   extraReducers: {
-    [modules_mailchimp_campaigns_update_campaign_settings_create.pending]: (
-      state,
-      action
-    ) => {
+    [modules_mailchimp_campaigns_update_campaign_settings_create.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
-    [modules_mailchimp_campaigns_update_campaign_settings_create.fulfilled]: (
-      state,
-      action
-    ) => {
+    [modules_mailchimp_campaigns_update_campaign_settings_create.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities.push(action.payload)
-        state.api.loading = "idle"
+        state.entities.push(action.payload);
+        state.api.loading = "idle";
       }
     },
-    [modules_mailchimp_campaigns_update_campaign_settings_create.rejected]: (
-      state,
-      action
-    ) => {
+    [modules_mailchimp_campaigns_update_campaign_settings_create.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     }
   }
-})
+});
 export default {
   modules_mailchimp_campaigns_update_campaign_settings_create,
   slice: updateCampaignSettingsSlice
-}
+};
