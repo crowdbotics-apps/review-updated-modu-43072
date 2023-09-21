b57e7703c334205dc714587bd6e8363ccff2f279
Crowdbotics <engineering@crowdbotics.com>
2023-09-21 12:24:05 +0000

    Update store.

--- store/reviewupdatedmoduAPI/files.slice.js
+++ store/reviewupdatedmoduAPI/files.slice.js
@@ -1,16 +1,15 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { apiService } from "./api";
-export const modules_slack_service_upload_file_create = createAsyncThunk("files/modules_slack_service_upload_file_create", async payload => {
-  const response = await apiService.modules_slack_service_upload_file_create(payload);
-  return response.data;
-});
-const initialState = {
-  entities: [],
-  api: {
-    loading: "idle",
-    error: null
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const modules_slack_service_upload_file_create = createAsyncThunk(
+  "files/modules_slack_service_upload_file_create",
+  async payload => {
+    const response = await apiService.modules_slack_service_upload_file_create(
+      payload
+    )
+    return response.data
   }
-};
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
 const filesSlice = createSlice({
   name: "files",
   initialState,
@@ -18,24 +17,21 @@ const filesSlice = createSlice({
   extraReducers: {
     [modules_slack_service_upload_file_create.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending";
+        state.api.loading = "pending"
       }
     },
     [modules_slack_service_upload_file_create.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities.push(action.payload);
-        state.api.loading = "idle";
+        state.entities.push(action.payload)
+        state.api.loading = "idle"
       }
     },
     [modules_slack_service_upload_file_create.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
+        state.api.error = action.error
+        state.api.loading = "idle"
       }
     }
   }
-});
-export default {
-  modules_slack_service_upload_file_create,
-  slice: filesSlice
-};
+})
+export default { modules_slack_service_upload_file_create, slice: filesSlice }
