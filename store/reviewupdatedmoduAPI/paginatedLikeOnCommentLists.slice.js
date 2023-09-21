509ad6f9022cdb4f89681e687c8166b9cb7ffa17
Saad bin Abid <saad.abid@crowdbotics.com>
2023-09-21 18:21:19 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/reviewupdatedmoduAPI/paginatedLikeOnCommentLists.slice.js
+++ store/reviewupdatedmoduAPI/paginatedLikeOnCommentLists.slice.js
@@ -1,13 +1,16 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { apiService } from "./api"
-export const modules_disqus_comment_like_list = createAsyncThunk(
-  "paginatedLikeOnCommentLists/modules_disqus_comment_like_list",
-  async payload => {
-    const response = await apiService.modules_disqus_comment_like_list(payload)
-    return response.data
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiService } from "./api";
+export const modules_disqus_comment_like_list = createAsyncThunk("paginatedLikeOnCommentLists/modules_disqus_comment_like_list", async payload => {
+  const response = await apiService.modules_disqus_comment_like_list(payload);
+  return response.data;
+});
+const initialState = {
+  entities: [],
+  api: {
+    loading: "idle",
+    error: null
   }
-)
-const initialState = { entities: [], api: { loading: "idle", error: null } }
+};
 const paginatedLikeOnCommentListsSlice = createSlice({
   name: "paginatedLikeOnCommentLists",
   initialState,
@@ -15,27 +18,24 @@ const paginatedLikeOnCommentListsSlice = createSlice({
   extraReducers: {
     [modules_disqus_comment_like_list.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [modules_disqus_comment_like_list.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = [
-          ...state.entities.filter(record => record.id !== action.payload.id),
-          action.payload
-        ]
-        state.api.loading = "idle"
+        state.entities = [...state.entities.filter(record => record.id !== action.payload.id), action.payload];
+        state.api.loading = "idle";
       }
     },
     [modules_disqus_comment_like_list.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     }
   }
-})
+});
 export default {
   modules_disqus_comment_like_list,
   slice: paginatedLikeOnCommentListsSlice
-}
+};
