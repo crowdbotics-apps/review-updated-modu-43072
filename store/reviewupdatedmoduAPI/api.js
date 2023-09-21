ad9162ab1440f52394ca19326002ecdb07e94915
Saad bin Abid <saad.abid@crowdbotics.com>
2023-09-21 16:25:57 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/reviewupdatedmoduAPI/api.js
+++ store/reviewupdatedmoduAPI/api.js
@@ -1,70 +1,88 @@
-import axios from "axios"
+import axios from "axios";
 const reviewupdatedmoduAPI = axios.create({
   baseURL: "https://review-updated-modu-43072.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return reviewupdatedmoduAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return reviewupdatedmoduAPI.post(`/api/v1/login/`, payload.data)
+  return reviewupdatedmoduAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return reviewupdatedmoduAPI.post(`/api/v1/signup/`, payload.data)
+  return reviewupdatedmoduAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function modules_zoominfo_auth_token_create(payload) {
-  return reviewupdatedmoduAPI.post(`/modules/zoominfo/auth/token/`)
+  return reviewupdatedmoduAPI.post(`/modules/zoominfo/auth/token/`);
 }
+
 function modules_zoominfo_data_bulk_create(payload) {
-  return reviewupdatedmoduAPI.post(`/modules/zoominfo/data/bulk/`)
+  return reviewupdatedmoduAPI.post(`/modules/zoominfo/data/bulk/`);
 }
+
 function modules_zoominfo_data_enrich_create(payload) {
-  return reviewupdatedmoduAPI.post(`/modules/zoominfo/data/enrich/`)
+  return reviewupdatedmoduAPI.post(`/modules/zoominfo/data/enrich/`);
 }
+
 function modules_zoominfo_data_search_create(payload) {
-  return reviewupdatedmoduAPI.post(`/modules/zoominfo/data/search/`)
+  return reviewupdatedmoduAPI.post(`/modules/zoominfo/data/search/`);
 }
+
 function rest_auth_login_create(payload) {
-  return reviewupdatedmoduAPI.post(`/rest-auth/login/`, payload.data)
+  return reviewupdatedmoduAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return reviewupdatedmoduAPI.get(`/rest-auth/logout/`)
+  return reviewupdatedmoduAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return reviewupdatedmoduAPI.post(`/rest-auth/logout/`)
+  return reviewupdatedmoduAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return reviewupdatedmoduAPI.post(`/rest-auth/password/change/`, payload.data)
+  return reviewupdatedmoduAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return reviewupdatedmoduAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return reviewupdatedmoduAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return reviewupdatedmoduAPI.post(
-    `/rest-auth/password/reset/confirm/`,
-    payload.data
-  )
+  return reviewupdatedmoduAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return reviewupdatedmoduAPI.post(`/rest-auth/registration/`, payload.data)
+  return reviewupdatedmoduAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return reviewupdatedmoduAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return reviewupdatedmoduAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return reviewupdatedmoduAPI.get(`/rest-auth/user/`)
+  return reviewupdatedmoduAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return reviewupdatedmoduAPI.put(`/rest-auth/user/`, payload.data)
+  return reviewupdatedmoduAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return reviewupdatedmoduAPI.patch(`/rest-auth/user/`, payload.data)
+  return reviewupdatedmoduAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -84,4 +102,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
