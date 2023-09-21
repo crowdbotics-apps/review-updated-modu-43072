0469529ef38b60ae16578e7d86c816824b1bda2c
Crowdbotics <engineering@crowdbotics.com>
2023-09-21 14:10:20 +0000

    Update store.

--- store/reviewupdatedmoduAPI/meetings.slice.js
+++ store/reviewupdatedmoduAPI/meetings.slice.js
@@ -1,41 +1,49 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { apiService } from "./api";
-export const modules_appointment_service_appointment_sync_create = createAsyncThunk("meetings/modules_appointment_service_appointment_sync_create", async payload => {
-  const response = await apiService.modules_appointment_service_appointment_sync_create(payload);
-  return response.data;
-});
-const initialState = {
-  entities: [],
-  api: {
-    loading: "idle",
-    error: null
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const modules_appointment_service_appointment_sync_create = createAsyncThunk(
+  "meetings/modules_appointment_service_appointment_sync_create",
+  async payload => {
+    const response = await apiService.modules_appointment_service_appointment_sync_create(
+      payload
+    )
+    return response.data
   }
-};
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
 const meetingsSlice = createSlice({
   name: "meetings",
   initialState,
   reducers: {},
   extraReducers: {
-    [modules_appointment_service_appointment_sync_create.pending]: (state, action) => {
+    [modules_appointment_service_appointment_sync_create.pending]: (
+      state,
+      action
+    ) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending";
+        state.api.loading = "pending"
       }
     },
-    [modules_appointment_service_appointment_sync_create.fulfilled]: (state, action) => {
+    [modules_appointment_service_appointment_sync_create.fulfilled]: (
+      state,
+      action
+    ) => {
       if (state.api.loading === "pending") {
-        state.entities.push(action.payload);
-        state.api.loading = "idle";
+        state.entities.push(action.payload)
+        state.api.loading = "idle"
       }
     },
-    [modules_appointment_service_appointment_sync_create.rejected]: (state, action) => {
+    [modules_appointment_service_appointment_sync_create.rejected]: (
+      state,
+      action
+    ) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
+        state.api.error = action.error
+        state.api.loading = "idle"
       }
     }
   }
-});
+})
 export default {
   modules_appointment_service_appointment_sync_create,
   slice: meetingsSlice
-};
+}
